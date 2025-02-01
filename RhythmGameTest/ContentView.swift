06d462e8768bf51f5613a4fbf53a5356989f3533
Oliver Heisel <Oliemanq456@gmail.com>
2025-01-31 18:11:36 -0600

    Reworked music handling again, no longer works :/

--- RhythmGameTest/ContentView.swift
+++ RhythmGameTest/ContentView.swift
@@ -10,71 +10,111 @@ import WatchConnectivity
 import MediaPlayer
 import Darwin
 
-var text = "Hello World ";
 let musicPlayer = MPMusicPlayerController.systemMusicPlayer
+var songTitle = musicPlayer.nowPlayingItem?.title ?? "No song playing"
 
+
+class MusicMonitor: ObservableObject {
+    private let player = MPMusicPlayerController.systemMusicPlayer
+    
+    init() {
+        NotificationCenter.default.addObserver(
+            forName: .MPMusicPlayerControllerNowPlayingItemDidChange,
+            object: player,
+            queue: OperationQueue.main) { (note) in
+                self.updateCurrentSong()
+                printTest(inp:("Song changed " + (self.player.nowPlayingItem?.title ?? "No song playing")))
+        }
+        
+        player.beginGeneratingPlaybackNotifications()
+        updateCurrentSong() // Get initial song
+    }
+    
+    private func updateCurrentSong() {
+        if let nowPlayingItem = player.nowPlayingItem {
+            songTitle = nowPlayingItem.title ?? "No song playing" // Your next song logic here
+        }
+    }
+    
+    deinit {
+        player.endGeneratingPlaybackNotifications()
+    }
+}
 
 struct ContentView: View {
+
     @State private var showAlert1 = false
     @State private var showAlert2 = false
-    @State private var musicPerms = false
+    @State private var musicPerms = MPMediaLibrary.authorizationStatus() == .authorized
     @State private var fontSize = UIScreen.main.bounds.size.height/30
-    @State private var songTitle = musicPlayer.nowPlayingItem?.title ?? "No song playing"
+    
 
-    
-    
-    
+
     var body: some View {
         
         VStack {
+            
             Text(songTitle)
+            .underline()
+            .frame(width: CGFloat(songTitle.count)*fontSize/1.4, height:UIScreen.main.bounds.size.height/20)
             .font(.system(size: fontSize))
-            .frame(width: CGFloat(songTitle.count)*fontSize/2, height:UIScreen.main.bounds.size.height/20)
             .foregroundColor(.secondary)
             .background(Color.clear)
-            .clipShape(Capsule())
-            .overlay(Capsule().stroke(.gray, lineWidth: 2))
             .shadow(color:.gray,radius: 3)
             .offset(y: UIScreen.main.bounds.size.height/5.1)
-            .opacity(musicPerms ? 1 : 0)
+            .opacity(musicPerms ? 1:0)
+            .padding(.vertical, 10)
+
             
             
             //BUTTON 1________________________________________________
-            Button("Display song title"){
-                requestMusicPermission()
-                fetchNowPlaying()
+            Button("Reset song title"){
                 showAlert1=true
             }.alert(songTitle, isPresented: $showAlert1) {
+                Button("Cancel", role:.cancel) { }
                 Button("Reset") {
                     songTitle = "No song playing"
                 }
-                Button("Ok", role:.cancel) { }
             }
             .frame(width: UIScreen.main.bounds.width/1.6, height:UIScreen.main.bounds.size.height/20)
             .font(.system(size: fontSize))
             .foregroundColor(.secondary)
             .background(Color.clear)
             .clipShape(Capsule())
-            .overlay(Capsule().stroke(.gray, lineWidth: 2))
+            .overlay(Capsule().stroke(.gray, lineWidth: 2.5))
             .shadow(color:.gray,radius: 3)
             .offset(y: UIScreen.main.bounds.size.height/5.1)
-            .opacity(musicPerms ? 1 : 0)
+            .opacity(musicPerms ? 1:0)
             
             //BUTTON 2_________________________________________________
-            Button("Request music access"){
-                showAlert2=true
-                requestMusicPermission()
+            if(!musicPerms){
+                Button("Request music access"){
+                    showAlert2=true
+                    requestMusicPermission()
+                }
+                .frame(width: CGFloat("request music access".count)*fontSize/2, height:UIScreen.main.bounds.size.height/20)
+                .font(.system(size: fontSize))
+                .foregroundColor(.secondary)
+                .background(Color.clear)
+                .clipShape(Capsule())
+                .overlay(Capsule().stroke(.gray, lineWidth: 2))
+                .shadow(color:.red,radius: 3)
+                .offset(y: UIScreen.main.bounds.size.height/5)
+            }else{
+                Text("Music permission granted")
+                    .frame(width: CGFloat("Music permission granted".count)*fontSize/2, height:UIScreen.main.bounds.size.height/20)
+                    .font(.system(size: fontSize))
+                    .foregroundColor(.secondary)
+                    .background(Color.clear)
+                    .clipShape(Capsule())
+                    .overlay(Capsule().stroke(.gray, lineWidth: 2))
+                    .shadow(color:.green,radius: 3)
+                    .offset(y: UIScreen.main.bounds.size.height/5)
             }
-            .frame(width: UIScreen.main.bounds.width/1.25, height:UIScreen.main.bounds.size.height/20)
-            .font(.system(size: fontSize))
-            .foregroundColor(.secondary)
-            .background(Color.clear)
-            .clipShape(Capsule())
-            .overlay(Capsule().stroke(.gray, lineWidth: 2))
-            .shadow(color:.gray,radius: 3)
-            .offset(y: UIScreen.main.bounds.size.height/5)
-        }.onAppear(perform: fetchNowPlaying)
+            
+        }
     }
+        
     func requestMusicPermission() {
         if MPMediaLibrary.authorizationStatus() == .denied {
             print("User has denied access to Apple Music. Direct them to Settings.")
@@ -82,8 +122,7 @@ struct ContentView: View {
             MPMediaLibrary.requestAuthorization { status in
                 switch status {
                 case .authorized:
-                    print("Access granted")
-                    musicPerms = true
+                    print("Access granted " + String(describing:musicPerms))
                 case .denied, .restricted:
                     print("Access denied")
                 case .notDetermined:
@@ -91,17 +130,6 @@ struct ContentView: View {
                 @unknown default:
                     fatalError("Unknown authorization status")
                 }
-            }
-        }
-    }
-    
-    func fetchNowPlaying() {
-        print("ran " + songTitle)
-        DispatchQueue.main.async {
-            if let nowPlayingItem = musicPlayer.nowPlayingItem {
-                songTitle = nowPlayingItem.title ?? "Unknown Title"
-            } else {
-                songTitle = "No Song Playing"
             }
         }
     }
@@ -113,8 +141,9 @@ struct ContentView: View {
     ContentView()
 }
 
-func printTest(){
-    print("Action called");
+func printTest(inp: String){
+    let input = inp;
+    print(input);
 }
 
 
